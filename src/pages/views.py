6f14013b170c8517bf1a23a9229f7f3045c2a577
Oleksandr Maistrenko <namelost@protonmail.com>
2020-10-11 13:16:09 +0300

    added templates/home.html

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, 'home.html', {})
 
 def contact_view(*args, **kwargs):
     return HttpResponse("<h1>Hey Contact Page!</h1>")
