c3eb23a4b20921774dddaf931403660575762665
Oleksandr Maistrenko <namelost@protonmail.com>
2020-10-11 18:32:54 +0300

    adding raw_product_form

--- src/products/views.py
+++ src/products/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-    
+    form = RawProductForm()
+    if request.method == "POST":
+        form = RawProductForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Product.objects.create(**form.cleaned_data)
+        else:
+            print(form.errors)
     context = {
         'form': form,
     }
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+    
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     prod = Product.objects.get(id=1)
